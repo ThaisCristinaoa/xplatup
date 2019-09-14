fc804571acde711a65deab9fbe0cd3c7f4575ef7
ThaisCristinaoa <52842898+ThaisCristinaoa@users.noreply.github.com>
2019-09-14 10:41:59 -0300

    🚧 setup

---
+++ src/XplatCollect/XplatCollect/XplatCollect/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Mvvm;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XplatCollect.ViewModels
+{
+    public sealed class HomePageViewModel : ViewModelBase
+    {
+        private string title;
+
+        public HomePageViewModel(INavigationService navigationService
+            , IPageDialogService pageDialogService)
+            : base(navigationService, pageDialogService)
+        {
+        }
+
+        public string Title
+        {
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+    }
+}
+}
